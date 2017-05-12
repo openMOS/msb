0a3113807aaaabf9688e170ee180a0adf2d649b6
as-filipe <andre.silva@introsys.eu>
2017-05-12 18:02:25 +0100

    new utils and scripts files

---
+++ utils/idl/generate_classes_linux.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+# <path to idlpp> -I <path to idl default> -l <language (e.g java)> <file ".idl">
+/opt/opensplice/V2.2.7/bin/idlpp -I /opt/opensplice/V2.2.7/etc/idl -l java "MSB2ADAPTER.idl"
+
