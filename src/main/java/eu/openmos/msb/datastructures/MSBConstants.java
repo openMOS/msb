97100e9ddefdd36b0aa12a62ec93fdf3e505d3a7
Unknown <fabio.miranda@introsys.eu>
2018-02-19 15:31:58 +0000

    added system and adapter stages. Refactor on msb constants

--- src/main/java/eu/openmos/msb/datastructures/MSBConstants.java
+++ src/main/java/eu/openmos/msb/datastructures/MSBConstants.java
@@ -10,7 +10,7 @@ package eu.openmos.msb.datastructures;
  * @author Introsys
  */
 public class MSBConstants {
-    
+
     public static final String ADAPTER_STATE_IDLE = "IDLE";
     public static final String ADAPTER_STATE_RUNNING = "Running";
     public static final String ADAPTER_STATE_READY = "Ready";
@@ -18,11 +18,26 @@ public class MSBConstants {
     public static final String ADAPTER_STATE_ERROR = "Error";
     public static final String ADAPTER_STATE_EMERGENCY_STOP = "EMERGENCY_STOP";
     
-    public static final String SYSTEM_STATE_RAMP_UP = "Ramp_Up";
-    public static final String SYSTEM_STATE_PRE_PRODUCTION = "Pre_Production";
-    public static final String SYSTEM_STATE_PRODUCTION = "Production";
-    
-    public static Boolean MSB_OPTIMIZER = true;
-    
-}
+    public static final String ADAPTER_STAGE_RAMP_UP = "Ramp_Up";
+    public static final String ADAPTER_STAGE_PRE_PRODUCTION = "Pre_Production";
+    public static final String ADAPTER_STAGE_PRODUCTION = "Production";
+    public static final String ADAPTER_STAGE_RAMP_UP_TO_PREPROD = "Rampup_to_pre-production";
+    public static final String ADAPTER_STAGE_RAMP_UP_TO_PRODUCTION = "Rampup_to_production";
+    public static final String ADAPTER_STAGE_PREPROD_TO_PRODUCTION = "Pre-production_to_production";
+    public static final String ADAPTER_STAGE_PREPROD_TO_RAMP_UP = "Pre-production_to_rampup";
+    public static final String ADAPTER_STAGE_PRODUCTION_TO_RAMP_UP = "Production_to_rampup";
+    public static final String ADAPTER_STAGE_PRODUCTION_TO_PREPROD = "Production_to_preproduction";
 
+    public static final String SYSTEM_STAGE_RAMP_UP = "Ramp_Up";
+    public static final String SYSTEM_STAGE_PRE_PRODUCTION = "Pre_Production";
+    public static final String SYSTEM_STAGE_PRODUCTION = "Production";
+    public static final String SYSTEM_STAGE_RAMP_UP_TO_PREPROD = "Rampup_to_pre-production";
+    public static final String SYSTEM_STAGE_RAMP_UP_TO_PRODUCTION = "Rampup_to_production";
+    public static final String SYSTEM_STAGE_PREPROD_TO_PRODUCTION = "Pre-production_to_production";
+    public static final String SYSTEM_STAGE_PREPROD_TO_RAMP_UP = "Pre-production_to_rampup";
+    public static final String SYSTEM_STAGE_PRODUCTION_TO_RAMP_UP = "Production_to_rampup";
+    public static final String SYSTEM_STAGE_PRODUCTION_TO_PREPROD = "Production_to_preproduction";
+
+    public static Boolean MSB_OPTIMIZER = true;
+
+}
